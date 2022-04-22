c17407d6a2e375273b5394a2a249877fc921687d
Vitalii <98636866+VitaliiChernelivskyi@users.noreply.github.com>
2022-04-22 17:31:55 +0300

    create components header

---
+++ src/components/V-Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <header>
+    <div class="header_wrapper_black">
+      <div class="header_wrapper_black_left">
+        <a href="">pricing</a>
+        <a href="">about us</a>
+        <a href="">faqs</a>
+      </div>
+      <div class="header_wrapper_black_right">
+        <a href="">register</a>
+        <a href="">Login</a>
+      </div>
+    </div>
+    <div class="header_wrapper_white">
+      <div class="header_wrapper_white_up">
+        <div>1</div>
+        <div>2</div>
+      </div>
+      <div class="header_wrapper_white_down">
+        <div>home</div>
+        <div>home</div>
+        <div>home</div>
+        <div>home</div>
+        <div>home</div>
+        <div>home</div>
+        <div>home</div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "V-Header",
+};
+</script>
+
+<style scoped lang="scss">
+.header_wrapper_black {
+  background: #020202;
+  display: flex;
+  color: white;
+  justify-content: space-between;
+
+  &-right {
+    display: flex;
+  }
+
+  &-left {
+    display: flex;
+  }
+}
+
+.header_wrapper_white_down {
+  display: flex;
+  justify-content: space-around;
+}
+.header_wrapper_white_up {
+  display: flex;
+  justify-content: space-around;
+}
+
+.header_wrapper_white {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,7 @@
 <template>
-  <div class="home">
-    <header>
-      <div class="header_wrapper_black">
-        <div class="header_wrapper_black_left">
-          <a href="">pricing</a>
-          <a href="">about us</a>
-          <a href="">faqs</a>
-        </div>
-        <div class="header_wrapper_black_right">
-          <a href="">register</a>
-          <a href="">Login</a>
-        </div>
-      </div>
-      <div class="header_wrapper_white">
-        <div class="header_wrapper_white_up">
-          <div>1</div>
-          <div>2</div>
-        </div>
-        <div class="header_wrapper_white_down">
-          <div>home</div>
-          <div>home</div>
-          <div>home</div>
-          <div>home</div>
-          <div>home</div>
-          <div>home</div>
-          <div>home</div>
-        </div>
-      </div>
-    </header>
-  </div>
+  <div class="home"></div>
 </template>
-<style scoped lang="scss">
-.header_wrapper_black {
-  background: #020202;
-  display: flex;
-  color: white;
-  justify-content: space-between;
-
-  &-right {
-    display: flex;
-  }
-
-  &-left {
-    display: flex;
-  }
-}
-
-.header_wrapper_white_down {
-  display: flex;
-  justify-content: space-around;
-}
-.header_wrapper_white_up {
-  display: flex;
-  justify-content: space-around;
-}
-
-.header_wrapper_white {
-  display: flex;
-  flex-direction: column;
-}
-</style>
+<style scoped lang="scss"></style>
 <script>
 export default {
   name: "HomeView",
