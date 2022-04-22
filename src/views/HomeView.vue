70f6968e59e02ac8333c129d32462d6aba056502
Vitalii <98636866+VitaliiChernelivskyi@users.noreply.github.com>
2022-04-22 17:23:50 +0300

    create header structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <header>
+      <div class="header_wrapper_black">
+        <div class="header_wrapper_black_left">
+          <a href="">pricing</a>
+          <a href="">about us</a>
+          <a href="">faqs</a>
+        </div>
+        <div class="header_wrapper_black_right">
+          <a href="">register</a>
+          <a href="">Login</a>
+        </div>
+      </div>
+      <div class="header_wrapper_white">
+        <div class="header_wrapper_white_up">
+          <div>1</div>
+          <div>2</div>
+        </div>
+        <div class="header_wrapper_white_down">
+          <div>home</div>
+          <div>home</div>
+          <div>home</div>
+          <div>home</div>
+          <div>home</div>
+          <div>home</div>
+          <div>home</div>
+        </div>
+      </div>
+    </header>
   </div>
 </template>
+<style scoped lang="scss">
+.header_wrapper_black {
+  background: #020202;
+  display: flex;
+  color: white;
 
+  &-right {
+    display: flex;
+  }
+
+  &-left {
+    display: flex;
+  }
+}
+
+.header_wrapper_white_down {
+  display: flex;
+  justify-content: space-around;
+}
+.header_wrapper_white_up {
+  display: flex;
+  justify-content: space-around;
+}
+
+.header_wrapper_white {
+  display: flex;
+  flex-direction: column;
+}
+</style>
 <script>
 export default {
   name: "HomeView",
