4a8276f09e77201914f329da8886b2395282d19e
Vitalii <98636866+VitaliiChernelivskyi@users.noreply.github.com>
2022-04-25 11:29:09 +0300

    footer fix position

--- src/components/VFooter.vue
+++ src/components/VFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer-bg">
-    <div class="row">
-      <div class="d-flex col-3">
+    <div class="d-flex justify-content-between">
+      <div class="d-flex flex-column">
         <div>information</div>
         <a href="">faqs</a>
         <a href="">our events</a>
@@ -9,7 +9,7 @@
         <a href="">our team</a>
         <a href="">contact us</a>
       </div>
-      <div class="d-flex col-3">
+      <div class="d-flex flex-column">
         <div>categories</div>
         <a href="">cat sitting</a>
         <a href="">cats</a>
@@ -20,10 +20,19 @@
       </div>
       <div>
         <div>recent posts</div>
+        <div>1</div>
+        <div>2</div>
       </div>
-      <div></div>
+      <div>
+        <div>get our newsletter</div>
+        <div>interdum et malesuada fames ac ante venenatis ipsum non.</div>
+        <div>
+          <input type="E-mail" />
+          <button>subscribe</button>
+        </div>
+      </div>
     </div>
-    <div class="footer_bottom">
+    <div class="footer_bottom d-flex justify-content-around">
       <div>2022. all rights reserved. done by dan fisher.</div>
       <div>keep in touch</div>
       <div></div>
