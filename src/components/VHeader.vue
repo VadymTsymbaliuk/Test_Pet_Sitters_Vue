e05f46fc504b280b921d3c58b75f5237249125bc
Vitalii <98636866+VitaliiChernelivskyi@users.noreply.github.com>
2022-04-27 12:31:54 +0300

    fix header and add bootstrap js

--- src/components/VHeader.vue
+++ src/components/VHeader.vue
@@ -1,46 +1,87 @@
 <template>
   <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="d-flex align-self-center navbar-brand" href="#"
+        ><img src="../../public/img/pet_sitter_logo.png" alt="" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >PRICING <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FAQS</a>
+          </li>
+          <!--          <li class="nav-item dropdown">-->
+          <!--            <a-->
+          <!--              class="nav-link dropdown-toggle"-->
+          <!--              href="#"-->
+          <!--              id="navbarDropdown"-->
+          <!--              role="button"-->
+          <!--              data-toggle="dropdown"-->
+          <!--              aria-expanded="false"-->
+          <!--            >-->
+          <!--              MENU-->
+          <!--            </a>-->
+          <!--            <div class="dropdown-menu" aria-labelledby="navbarDropdown">-->
+          <!--              <a class="dropdown-item" href="#">Action</a>-->
+          <!--              <a class="dropdown-item" href="#">Another action</a>-->
+          <!--              <div class="dropdown-divider"></div>-->
+          <!--              <a class="dropdown-item" href="#">Something else here</a>-->
+          <!--            </div>-->
+          <!--          </li>-->
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <div class="header_wrapper_black_right">
+            <a class="pr-2" href=""> REGISTER</a>
+            <a href=""><span class="icon-icons8-youtube"></span> LOGIN</a>
+          </div>
+        </form>
+      </div>
+    </nav>
     <div class="header_wrapper_black">
-      <div class="header_wrapper_black_left">
-        <a href=""> PRICING</a>
-        <a href="">ABOUT US</a>
-        <a href="">FAQS</a>
-      </div>
-      <div class="header_wrapper_black_right">
-        <a href=""> REGISTER</a>
-        <a href=""><span class="icon-icons8-youtube"></span> LOGIN</a>
-      </div>
+      <a href="#">HOME</a>
+      <a href="#">PAGES</a>
+      <a href="#">SHORTCODES</a>
+      <a href="#">JOBS</a>
+      <a href="#">SITTERS</a>
+      <a href="#">BLOG</a>
+      <a href="#">PORTFOLIO</a>
+      <a href="#">CONTACTS</a>
     </div>
-    <div class="header_wrapper_white">
-      <div class="header_wrapper_white_up">
-        <div>
-          <img src="../../public/img/pet_sitter_logo.png" alt="" />
-          <div><i>Find an Awesome Aet Sitter</i></div>
-        </div>
-        <div class="d-flex flex-column">
-          <div class="pb-2">
-            <a class="p-2" href=""> <b>call us</b> : + 1(234)567890</a>
-            <a class="p-2" href=""><b>Email: </b>petsitter@dan-fisher.com</a>
-          </div>
-          <div class="d-flex justify-content-center">
-            <img class="pr-2" src="../../public/img/google-plus.png" alt="" />
-            <img class="pr-2" src="../../public/img/instagram.png" alt="" />
-            <img class="pr-2" src="../../public/img/facebook.png" alt="" />
-            <img class="pr-2" src="../../public/img/twitter.png" alt="" />
-          </div>
-        </div>
-      </div>
-      <div class="header_wrapper_white_down mb-4">
-        <div>HOME</div>
-        <div>PAGES</div>
-        <div>SHORTCODES</div>
-        <div>JOBS</div>
-        <div>SITTERS</div>
-        <div>BLOG</div>
-        <div>PORTFOLIO</div>
-        <div>CONTACTS</div>
-      </div>
-    </div>
+    <!--    <div class="header_wrapper_white">-->
+    <!--      <div class="header_wrapper_white_up">-->
+    <!--        <div class="d-flex flex-column">-->
+    <!--          <div>-->
+    <!--            <a class="" href=""> <b>call us</b> : + 1(234)567890</a>-->
+    <!--            <a class="p-2" href=""><b>Email: </b>petsitter@dan-fisher.com</a>-->
+    <!--          </div>-->
+    <!--          <div class="d-flex mt-1 justify-content-around">-->
+    <!--            <img class="pr-2" src="../../public/img/google-plus.png" alt="" />-->
+    <!--            <img class="pr-2" src="../../public/img/instagram.png" alt="" />-->
+    <!--            <img class="pr-2" src="../../public/img/facebook.png" alt="" />-->
+    <!--            <img class="pr-2" src="../../public/img/twitter.png" alt="" />-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </div>-->
   </header>
 </template>
 
@@ -52,33 +93,15 @@ export default {
 
 <style scoped lang="scss">
 .header_wrapper_black {
-  background: #020202;
+  background: #686868;
   display: flex;
   color: white;
-  height: 60px;
+  min-height: 40px;
   justify-content: space-around;
   align-items: center;
 
-  &-right {
-    display: flex;
-  }
-
-  &-left {
-    display: flex;
-  }
-
   & a {
     color: white;
-  }
-}
-.header_wrapper_black_left {
-  a {
-    padding: 10px;
-  }
-}
-.header_wrapper_black_right {
-  a {
-    padding: 10px;
   }
 }
 
@@ -88,8 +111,8 @@ export default {
 }
 
 .header_wrapper_white_up {
-  padding-top: 80px;
-  padding-bottom: 50px;
+  padding-top: 10px;
+  padding-bottom: 10px;
   display: flex;
   justify-content: space-around;
 }
@@ -105,6 +128,11 @@ export default {
     padding-bottom: 5px;
     display: flex;
     flex-wrap: wrap;
+  }
+  .header_wrapper_black {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px;
   }
   .header_wrapper_white_down {
     display: flex;
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-
+import "bootstrap/dist/js/bootstrap.min";
 import "./assets/scss/main.scss";
 
 createApp(App).use(store).use(router).mount("#app");
