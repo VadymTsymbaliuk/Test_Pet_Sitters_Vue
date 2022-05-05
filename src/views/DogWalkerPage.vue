ee4e44db5aaf4377b5b1fd5f008b5ad503f0f6a5
vadimcimbalyk <vadymonua@gmail.com>
2022-05-05 17:01:59 +0300

    add mapbox

--- src/views/DogWalkerPage.vue
+++ src/views/DogWalkerPage.vue
@@ -1,17 +1,200 @@
 <template>
-  <div>
-    sdfg
-    <main class="w-screen h-screen">
-      <v-map class="w-full h-full" :options="state.map">
-        <MglAttributionControl />
-        <MglNavigationControl position="top-right" />
-        <MglGeolocateControl position="top-right" />
-        <MglNavigationControl position="top-right" />
-        <MglGeolocateControl position="top-right" />
-        <MglScaleControl />
-        <MglFullscreenControl />
-      </v-map>
-    </main>
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+        <div class="row sitter-container">
+          <div class="col">
+            <img src="../assets/img/sitter_avatar_1.png" alt="sitter avatar" />
+          </div>
+          <div class="col">
+            <div class="sitter-info">
+              <h2>niki belk</h2>
+              <p>walk, play and care</p>
+            </div>
+            <div class="all-info">
+              <p>dog walker</p>
+              <p>denver uS</p>
+              <p>posted 4 years ago</p>
+            </div>
+            <div class="contact-info">
+              <p>webcite</p>
+              <p>denfiser_dev</p>
+            </div>
+          </div>
+        </div>
+        <div class="description-container">
+          <h2>Description</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+            animi asperiores atque consectetur debitis dolorum eligendi est et
+            mollitia nam natus nihil obcaecati, praesentium quia quod sed
+            tempore ullam vero.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+            adipisci alias, cumque deserunt dolorem eum explicabo fugit hic
+            magnam nobis quaerat quasi quis, quisquam saepe sapiente tempora
+            velit. Nemo, sed!
+          </p>
+        </div>
+        <div class="location-container">
+          <v-map class="map" :options="state.map">
+            <MglAttributionControl />
+            <MglNavigationControl position="top-right" />
+            <MglGeolocateControl position="top-right" />
+            <MglNavigationControl position="top-right" />
+            <MglGeolocateControl position="top-right" />
+            <MglScaleControl />
+            <MglFullscreenControl />
+          </v-map>
+        </div>
+        <div class="sitter-characteristics">
+          <div>
+            <h3>Skils</h3>
+            <ul>
+              <li>Skil</li>
+              <li>Skil</li>
+              <li>Skil</li>
+              <li>Skil</li>
+              <li>Skil</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Experiense</h3>
+            <ul>
+              <li>Experiense</li>
+              <li>Experiense</li>
+              <li>Experiense</li>
+              <li>Experiense</li>
+              <li>Experiense</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Education</h3>
+            <ul>
+              <li>Education</li>
+              <li>Education</li>
+              <li>Education</li>
+              <li>Education</li>
+              <li>Education</li>
+            </ul>
+          </div>
+        </div>
+        <div class="user-comments">
+          <h2>What people say?</h2>
+          <div class="sort-container">
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Recipient's username"
+                aria-label="Recipient's username"
+                aria-describedby="button-addon2"
+              />
+              <div class="input-group-append">
+                <button
+                  class="btn btn-outline-secondary"
+                  type="button"
+                  id="button-addon2"
+                >
+                  Button
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="comments-container">
+            <div class="comment-item">
+              <img
+                src="https://img.icons8.com/plasticine/100/000000/user.png"
+              />
+              <div class="d-flex flex-row">
+                <div>
+                  <div class="stars-container">1111</div>
+                  <p>cool</p>
+                  <p>test</p>
+                  <p>june 23, 2022, 12:45 pm</p>
+                </div>
+              </div>
+              <div class="rating-container">
+                <div class="like">
+                  <button class="btn">
+                    <img
+                      src="https://img.icons8.com/external-those-icons-fill-those-icons/24/000000/external-like-feedback-those-icons-fill-those-icons.png"
+                    />
+                  </button>
+                </div>
+                <div class="dislike">
+                  <button class="btn">
+                    <img
+                      src="https://img.icons8.com/glyph-neue/64/000000/poor-quality.png"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="comment-item">
+              <img
+                src="https://img.icons8.com/plasticine/100/000000/user.png"
+              />
+              <div class="d-flex flex-row">
+                <div>
+                  <div class="stars-container">1111</div>
+                  <p>cool</p>
+                  <p>test</p>
+                  <p>june 23, 2022, 12:45 pm</p>
+                </div>
+              </div>
+              <div class="rating-container">
+                <div class="like">
+                  <button class="btn">
+                    <img
+                      src="https://img.icons8.com/external-those-icons-fill-those-icons/24/000000/external-like-feedback-those-icons-fill-those-icons.png"
+                    />
+                  </button>
+                </div>
+                <div class="dislike">
+                  <button class="btn">
+                    <img
+                      src="https://img.icons8.com/glyph-neue/64/000000/poor-quality.png"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="comment-item">
+              <img
+                src="https://img.icons8.com/plasticine/100/000000/user.png"
+              />
+              <div class="d-flex flex-row">
+                <div>
+                  <div class="stars-container">1111</div>
+                  <p>cool</p>
+                  <p>test</p>
+                  <p>june 23, 2022, 12:45 pm</p>
+                </div>
+              </div>
+              <div class="rating-container">
+                <div class="like">
+                  <button class="btn">
+                    <img
+                      src="https://img.icons8.com/external-those-icons-fill-those-icons/24/000000/external-like-feedback-those-icons-fill-those-icons.png"
+                    />
+                  </button>
+                </div>
+                <div class="dislike">
+                  <button class="btn">
+                    <img
+                      src="https://img.icons8.com/glyph-neue/64/000000/poor-quality.png"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-3"></div>
+    </div>
   </div>
 </template>
 
@@ -79,32 +262,47 @@ export default {
 @import "~mapbox-gl/dist/mapbox-gl.css";
 @import "~v-mapbox/dist/v-mapbox.css";
 
-html,
-body {
-  margin: 0;
+.location-container {
+  width: 667px;
+  height: 255px;
 }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-.w-screen {
-  width: 100vw;
-}
-
-.h-screen {
-  height: 100vh;
-}
-
-.h-full {
+.map {
+  width: 100%;
   height: 100%;
 }
 
-.w-full {
-  width: 100%;
+.sitter-container {
+  img {
+    width: 100%;
+  }
+}
+.sitter-characteristics {
+  display: flex;
+  justify-content: space-between;
+  ul {
+    list-style: none;
+  }
+}
+
+.comment-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+}
+
+.rating-container {
+  display: flex;
+  align-items: end;
+
+  .btn {
+    width: 15px;
+    height: 15px;
+
+    img {
+      width: 15px;
+    }
+  }
 }
 </style>
