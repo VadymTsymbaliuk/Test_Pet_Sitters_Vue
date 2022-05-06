51865212c4e1b54f0ca92c6a3e86e68714c0c560
vadimcimbalyk <vadymonua@gmail.com>
2022-05-06 11:35:42 +0300

    recent resumes block

--- src/views/DogWalkerPage.vue
+++ src/views/DogWalkerPage.vue
@@ -1,12 +1,13 @@
 <template>
+  <BreadCrumb :crumbs="crumbs" :page-title="pageTitle" />
   <div class="container">
     <div class="row">
-      <div class="col-8">
+      <div class="col-7">
         <div class="row sitter-container">
-          <div class="col">
+          <div class="col img-container">
             <img src="../assets/img/sitter_avatar_1.png" alt="sitter avatar" />
           </div>
-          <div class="col">
+          <div class="col ml-5 d-flex flex-column justify-content-center">
             <div class="sitter-info">
               <h2>niki belk</h2>
               <p>walk, play and care</p>
@@ -54,53 +55,53 @@
           </div>
         </div>
         <div class="sitter-characteristics">
-          <div>
-            <h3>Skils</h3>
-            <ul>
-              <li>Skil</li>
-              <li>Skil</li>
-              <li>Skil</li>
-              <li>Skil</li>
-              <li>Skil</li>
-            </ul>
+          <div class="row">
+            <div class="col">
+              <h3>Skils</h3>
+              <ul>
+                <li>Skil</li>
+                <li>Skil</li>
+                <li>Skil</li>
+                <li>Skil</li>
+                <li>Skil</li>
+              </ul>
+            </div>
+            <div class="col">
+              <h3>Experiense</h3>
+              <ul>
+                <li>Experiense</li>
+                <li>Experiense</li>
+                <li>Experiense</li>
+                <li>Experiense</li>
+                <li>Experiense</li>
+              </ul>
+            </div>
+            <div class="col">
+              <h3>Education</h3>
+              <ul>
+                <li>Education</li>
+                <li>Education</li>
+                <li>Education</li>
+                <li>Education</li>
+                <li>Education</li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <h3>Experiense</h3>
-            <ul>
-              <li>Experiense</li>
-              <li>Experiense</li>
-              <li>Experiense</li>
-              <li>Experiense</li>
-              <li>Experiense</li>
-            </ul>
-          </div>
-          <div>
-            <h3>Education</h3>
-            <ul>
-              <li>Education</li>
-              <li>Education</li>
-              <li>Education</li>
-              <li>Education</li>
-              <li>Education</li>
-            </ul>
-          </div>
+          <button class="btn btn-info">contact</button>
         </div>
         <div class="row user-comments">
           <div class="col">
             <h2>What people say?</h2>
-            <div class="sort-container">
-              <div class="select-container">
-                <p>Sort by:</p>
-                <select>
-                  <option value="anyCategory" label selected>
-                    Any category
-                  </option>
-                  <option value="cat">Cat sitter</option>
-                  <option value="dog">Dog sitter</option>
-                  <option value="bird">Bird sitter</option>
-                </select>
-              </div>
+            <div class="select-container">
+              <p>Sort by:</p>
+              <select>
+                <option value="anyCategory" label selected>Any category</option>
+                <option value="cat">Cat sitter</option>
+                <option value="dog">Dog sitter</option>
+                <option value="bird">Bird sitter</option>
+              </select>
             </div>
+
             <div class="comments-container">
               <div class="comment-item">
                 <img
@@ -191,13 +192,23 @@
               </div>
             </div>
           </div>
+          <div class="col text-center mt-5 pt-3">
+            <button class="btn text-uppercase font-weight-bold">
+              show more
+            </button>
+          </div>
         </div>
       </div>
-      <div class="col-3">
+      <div class="col-4">
         <h2>Recent Resumes</h2>
-        <div class="row row-cols-1">
-          <div class="col">
-            <img src="" alt="" />
+        <div class="row row-cols-1 resume-container">
+          <div class="col resume-item">
+            <div class="img-container">
+              <img
+                src="../assets/img/sitter_avatar_1.png"
+                alt="sitter-avatar"
+              />
+            </div>
             <div>
               <p>niki belk</p>
               <p>walk, play and care</p>
@@ -206,8 +217,13 @@
               <p>new york</p>
             </div>
           </div>
-          <div class="col">
-            <img src="" alt="" />
+          <div class="col resume-item">
+            <div class="img-container">
+              <img
+                src="../assets/img/sitter_avatar_1.png"
+                alt="sitter-avatar"
+              />
+            </div>
             <div>
               <p>niki belk</p>
               <p>walk, play and care</p>
@@ -216,8 +232,13 @@
               <p>new york</p>
             </div>
           </div>
-          <div class="col">
-            <img src="" alt="" />
+          <div class="col resume-item">
+            <div class="img-container">
+              <img
+                src="../assets/img/sitter_avatar_1.png"
+                alt="sitter-avatar"
+              />
+            </div>
             <div>
               <p>niki belk</p>
               <p>walk, play and care</p>
@@ -226,8 +247,13 @@
               <p>new york</p>
             </div>
           </div>
-          <div class="col">
-            <img src="" alt="" />
+          <div class="col resume-item">
+            <div class="img-container">
+              <img
+                src="../assets/img/sitter_avatar_1.png"
+                alt="sitter-avatar"
+              />
+            </div>
             <div>
               <p>niki belk</p>
               <p>walk, play and care</p>
@@ -236,8 +262,13 @@
               <p>new york</p>
             </div>
           </div>
-          <div class="col">
-            <img src="" alt="" />
+          <div class="col resume-item">
+            <div class="img-container">
+              <img
+                src="../assets/img/sitter_avatar_1.png"
+                alt="sitter-avatar"
+              />
+            </div>
             <div>
               <p>niki belk</p>
               <p>walk, play and care</p>
@@ -246,14 +277,19 @@
               <p>new york</p>
             </div>
           </div>
-          <div class="col">
-            <img src="" alt="" />
-            <div>
-              <p>niki belk</p>
-              <p>walk, play and care</p>
+          <div class="col resume-item">
+            <div class="img-container">
+              <img
+                src="../assets/img/sitter_avatar_1.png"
+                alt="sitter-avatar"
+              />
             </div>
             <div>
-              <p>new york</p>
+              <h3>niki belk</h3>
+              <div>
+                <p>walk, play and care</p>
+                <p>new york</p>
+              </div>
             </div>
           </div>
         </div>
@@ -275,10 +311,12 @@ import {
   MglScaleControl,
 } from "v-mapbox";
 import { reactive } from "vue";
+import BreadCrumb from "@/components/BreadCrumb";
 
 export default {
   name: "DogWalkerPage",
   components: {
+    BreadCrumb,
     VMap,
     MglAttributionControl,
     MglNavigationControl,
@@ -291,6 +329,19 @@ export default {
       accessToken:
         "pk.eyJ1IjoidmFkeW10c3ltYmFsaXVrIiwiYSI6ImNsMHFyY3psczI3cGozanB3M3I0dGY0bnUifQ.3tBov4aH3A1qe69cnat-0w",
       style: "mapbox://styles/mapbox/streets-v11",
+      crumbs: [
+        {
+          title: "Home",
+          path: "/",
+        },
+        {
+          title: "Dog walker",
+          path: "dog-walker",
+        },
+      ],
+      pageTitle: {
+        title: "Looking for a dog walker ",
+      },
     };
   },
   setup() {
@@ -326,10 +377,41 @@ export default {
 @import "~mapbox-gl/dist/mapbox-gl.css";
 @import "~v-mapbox/dist/v-mapbox.css";
 
+.container > .row {
+  gap: 30px;
+
+  > .col-4 {
+    margin-top: 100px;
+
+    .resume-container {
+      margin-top: 42px;
+
+      .resume-item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #f3f5eb;
+        border: 1px solid #e5e5e5;
+        padding: 16px 20px;
+        cursor: pointer;
+        .img-container {
+          border-radius: 50%;
+          background-color: #fff;
+          overflow: hidden;
+        }
+        div {
+          height: 100%;
+        }
+      }
+    }
+  }
+}
+
 .location-container {
   .map {
     width: 100%;
     height: 265px;
+    margin-top: 60px;
   }
 }
 
@@ -337,6 +419,7 @@ export default {
 .description-container,
 .location-container {
   position: relative;
+
   &:before {
     position: absolute;
     content: "";
@@ -347,8 +430,62 @@ export default {
     background-color: #f3f5eb;
   }
 }
+
+.description-container {
+  padding-top: 26px;
+  padding-bottom: 64px;
+}
+
+.location-container {
+  padding-top: 100px;
+  padding-bottom: 110px;
+}
+
+.sitter-container {
+  padding-top: 120px;
+  padding-bottom: 26px;
+
+  .img-container {
+    padding: 32px 14px;
+    background-color: #fff;
+
+    img {
+      width: 100%;
+    }
+  }
+}
+
+.sitter-characteristics {
+  padding-top: 24px;
+  padding-bottom: 100px;
+
+  .col {
+    text-align: center;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  .btn {
+    display: block;
+    margin: 0 auto;
+    text-transform: capitalize;
+
+    &-info {
+      background-color: #39b3ce;
+      width: 160px;
+      padding: 10px;
+    }
+  }
+}
+
 .user-comments {
   position: relative;
+  padding: 100px 0;
+  flex-direction: column;
+
   &:before {
     position: absolute;
     content: "";
@@ -358,62 +495,61 @@ export default {
     height: 100%;
     background-color: #ececec;
   }
-  .select-container {
-    display: flex;
-    align-items: center;
-    select {
-      padding: 10px;
-      width: 230px;
+
+  .col {
+    padding: 0;
+
+    .select-container {
+      display: flex;
+      align-items: center;
+      margin-top: 32px;
+
+      select {
+        padding: 10px;
+        width: 230px;
+      }
+
+      p {
+        margin-right: 20px;
+        margin-bottom: 0;
+        font-family: "Roboto", sans-serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 15px;
+        line-height: 18px;
+        letter-spacing: 0.05em;
+        text-transform: capitalize;
+      }
     }
-    p {
-      margin-right: 20px;
-      margin-bottom: 0;
-      font-family: "Roboto", sans-serif;
-      font-style: normal;
-      font-weight: 600;
-      font-size: 15px;
-      line-height: 18px;
-      letter-spacing: 0.05em;
-      text-transform: capitalize;
-    }
-  }
-}
 
-.sitter-container {
-  img {
-    width: 100%;
-  }
-}
-.sitter-characteristics {
-  display: flex;
-  justify-content: space-between;
-  ul {
-    list-style: none;
-  }
-}
+    .comments-container {
+      display: flex;
+      flex-direction: column;
+      margin-top: 87px;
 
-.comments-container {
-  display: flex;
-  flex-direction: column;
-}
+      .comment-item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: row;
+        background-color: white;
+        padding: 38px 31px 43px 55px;
+        border: 2px solid #c4c4c4;
 
-.comment-item {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: row;
-}
+        .rating-container {
+          display: flex;
+          align-items: end;
 
-.rating-container {
-  display: flex;
-  align-items: end;
+          .btn {
+            width: 15px;
+            height: 15px;
 
-  .btn {
-    width: 15px;
-    height: 15px;
-
-    img {
-      width: 15px;
+            img {
+              width: 15px;
+            }
+          }
+        }
+      }
     }
   }
 }
