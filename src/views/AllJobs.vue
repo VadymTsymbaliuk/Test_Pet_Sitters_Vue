fb4036049897e5a162a8d8b3ed0fd990632b6783
vadimcimbalyk <vadymonua@gmail.com>
2022-05-10 16:54:58 +0300

    add breadcrumbs

--- src/views/AllJobs.vue
+++ src/views/AllJobs.vue
@@ -1,28 +1,49 @@
 <template>
-  <section class="mt-4 mb-4 container">
-    <form-search />
-    <div class="row">
-      <div class="col-12 col-sm-6">
-        <OvnerGridIteam />
-        <OvnerGridIteam />
-        <OvnerGridIteam />
+  <div>
+    <BreadCrumb :crumbs="crumbs" :page-title="pageTitle" />
+    <section class="mt-4 mb-4 container">
+      <form-search />
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <OvnerGridIteam />
+          <OvnerGridIteam />
+          <OvnerGridIteam />
+        </div>
+        <div class="col-12 col-sm-6">
+          <OvnerGridIteam />
+          <OvnerGridIteam />
+          <OvnerGridIteam />
+        </div>
       </div>
-      <div class="col-12 col-sm-6">
-        <OvnerGridIteam />
-        <OvnerGridIteam />
-        <OvnerGridIteam />
-      </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
 import OvnerGridIteam from "@/components/OvnerGridIteam";
 import FormSearch from "@/components/FormSearch";
+import BreadCrumb from "@/components/BreadCrumb";
 
 export default {
   name: "AllJobs",
-  components: { OvnerGridIteam, FormSearch },
+  components: { OvnerGridIteam, FormSearch, BreadCrumb },
+  data() {
+    return {
+      crumbs: [
+        {
+          title: "Home",
+          path: "/",
+        },
+        {
+          title: "All Jobs",
+          path: "",
+        },
+      ],
+      pageTitle: {
+        title: "All jobs",
+      },
+    };
+  },
 };
 </script>
 
--- src/views/VJobs.vue
+++ src/views/VJobs.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="my-4 container">
-    <form-search />
-    <card />
-    <card />
-    <card />
-    <card />
+  <div>
+    <BreadCrumb :crumbs="crumbs" :page-title="pageTitle" />
+    <div class="my-4 container">
+      <form-search />
+      <card />
+      <card />
+      <card />
+      <card />
+    </div>
   </div>
 </template>
 
 <script>
 import Card from "@/components/Card";
 import FormSearch from "@/components/FormSearch";
+import BreadCrumb from "@/components/BreadCrumb";
 
 export default {
   name: "VJobs",
-  components: { Card, FormSearch },
+  components: { Card, FormSearch, BreadCrumb },
+  data() {
+    return {
+      crumbs: [
+        {
+          title: "Home",
+          path: "/",
+        },
+        {
+          title: "Jobs",
+          path: "all-jobs",
+        },
+      ],
+      pageTitle: {
+        title: "Jobs",
+      },
+    };
+  },
 };
 </script>
 
